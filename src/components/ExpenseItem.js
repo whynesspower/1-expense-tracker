d3f6329a59c82cbd17e52d80ee8bc543d25e230b
Yashraj Shukla <yashrajshukla48@gmail.com>
2023-03-23 10:10:31 +0530

    Adding expense item

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,10 @@
+export default function ExpenseItem() {
+  return (
+    <div>
+      <div> March 23rd, 2023</div>
+      <div>
+        <h2>Car Insurance </h2>;<div>$294.93</div>
+      </div>
+    </div>
+  );
+}
