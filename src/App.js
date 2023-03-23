0212d50fdb854b31c08c4a5c8442880db9a024b7
Yashraj Shukla <yashrajshukla48@gmail.com>
2023-03-24 01:23:37 +0530

    Updated Dummy Values

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import NewExpense from './components/NewExpense/NewExpense';
-import Expenses from './components/Expenses/Expenses';
+import NewExpense from "./components/NewExpense/NewExpense";
+import Expenses from "./components/Expenses/Expenses";
 
 const DUMMY_EXPENSES = [
   {
-    id: 'e1',
-    title: 'Toilet Paper',
-    amount: 94.12,
+    id: "e1",
+    title: "Dinner in Ahmedabad",
+    amount: 130,
     date: new Date(2020, 7, 14),
   },
-  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
   {
-    id: 'e3',
-    title: 'Car Insurance',
-    amount: 294.67,
-    date: new Date(2021, 2, 28),
+    id: "e3",
+    title: "Vada Pao",
+    amount: 30,
+    date: new Date(2020, 2, 28),
   },
   {
-    id: 'e4',
-    title: 'New Desk (Wooden)',
+    id: "e4",
+    title: "Bus Fare",
+    amount: 450,
+    date: new Date(2020, 5, 12),
+  },
+  {
+    id: "e4",
+    title: "Train Fare Fare",
+    amount: 450,
+    date: new Date(2023, 5, 12),
+  },
+  {
+    id: "e4",
+    title: "Auto Rickshaw Fare",
     amount: 450,
     date: new Date(2021, 5, 12),
   },
