76e2747372328f35fd37c409f2454dda733fd2dd
Yashraj Shukla <yashrajshukla48@gmail.com>
2023-03-23 23:02:31 +0530

    Module 4 completed!

--- src/components/NewExpense.js
+++ src/components/NewExpense.js
@@ -1,10 +1,18 @@
 import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm.js";
 
-const NewExpense = () => {
+const NewExpense = (props) => {
+  const saveExpenseHandler = (enteredData) => {
+    const expenseDate = {
+      ...enteredData,
+      id: Math.random().toString(),
+    };
+    // console.log(expenseDate);
+    props.onAddExpense(expenseDate);
+  };
   return (
     <div className="new-expense">
-      <ExpenseForm />
+      <ExpenseForm onSaveExpenseData={saveExpenseHandler} />
     </div>
   );
 };
