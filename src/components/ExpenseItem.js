7cd31509f1419e967d46a724f4da9f14d389f604
Yashraj Shukla <yashrajshukla48@gmail.com>
2023-03-23 10:25:04 +0530

    Adding CSS to expenseItem

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,12 @@
+import "./ExpenseItem.css";
+
 export default function ExpenseItem() {
   return (
-    <div>
+    <div className="expense-item">
       <div> March 23rd, 2023</div>
-      <div>
-        <h2>Car Insurance </h2>;<div>$294.93</div>
+      <div className="expense-item__description">
+        <h2>Car Insurance </h2>
+        <div className="expense-item__price">$294.93</div>
       </div>
     </div>
   );
