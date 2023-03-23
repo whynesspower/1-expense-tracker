390b34588d104d3f29931b7fbfe1ed13f1a0f22f
Yashraj Shukla <yashrajshukla48@gmail.com>
2023-03-23 20:46:47 +0530

    adding form support and preventDefault

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,20 +1,47 @@
+import { useCallback, useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
+  const [enteredtTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEneteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+
+  function titleChangeHandler(event) {
+    setEnteredTitle(event.target.value);
+  }
+  const AmtChngHandler = (e) => {
+    setEneteredAmount(e.target.value);
+  };
+  const DtChngeHandler = (e) => {
+    setEnteredDate(e.target.value);
+  };
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const expenseData = {
+      title: enteredtTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+  };
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text"></input>
+          <input type="text" onChange={titleChangeHandler}></input>
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number"></input>
+          <input type="number" onChange={AmtChngHandler}></input>
         </div>
         <div className="new-expense__control">
           <label>Date </label>
-          <input type="date" min="2022-01-01" max="2023-03-23"></input>
+          <input
+            type="date"
+            min="2022-01-01"
+            max="2023-03-23"
+            onChange={DtChngeHandler}
+          ></input>
         </div>
       </div>
       <div className="new-expense__actions">
