55136c1a06cd93c0fba4d6b7c5ef67c567cccc88
Yashraj Shukla <yashrajshukla48@gmail.com>
2023-03-23 19:57:10 +0530

    creating expense form

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,6 +1,26 @@
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
-  return <form></form>;
+  return (
+    <form>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label>Title</label>
+          <input type="text"></input>
+        </div>
+        <div className="new-expense__control">
+          <label>Amount</label>
+          <input type="number"></input>
+        </div>
+        <div className="new-expense__control">
+          <label>Date </label>
+          <input type="date" min="2022-01-01" max="2023-03-23"></input>
+        </div>
+      </div>
+      <div className="new-expense__actions">
+        <button type="submit"> Add Expense</button>
+      </div>
+    </form>
+  );
 };
 export default ExpenseForm;
